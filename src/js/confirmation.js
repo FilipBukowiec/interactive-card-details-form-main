45c38d4a3e545bef3c93cafabc0e99f1354f7bd3
Filip Bukowiec <filip.bukowiec@interia.pl>
2022-11-20 14:50:35 +0100

    js new

--- src/js/confirmation.js
+++ src/js/confirmation.js
@@ -1,13 +1,12 @@
 
 
-const form = document.getElementById("Form");
+// const form = document.getElementById("Formdiv");
 
-export const Confirmation = function () {
-    console.log("jupi");
-    form.innerHTML = `<div class="thanks"><div class="accept"></div><h2>THANK YOU!</h2> <p> We've added your card details</p><button class="continue" id="Continue">Continue</button></div>`;
-    const Continue = document.getElementById("Continue");
-    Continue.addEventListener("click", () => location.reload());
-}
+// export const Confirmation = function () {
+//     form.innerHTML = `<div class="thanks"><div class="accept"></div><h2>THANK YOU!</h2> <p> We've added your card details</p><button class="continue" id="Continue">Continue</button></div>`;
+//     const Continue = document.getElementById("Continue");
+//     Continue.addEventListener("click", () => location.reload());
+// }
 
 
 
--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,39 @@
-import { Confirmation } from "./confirmation.js";
-const Confirmbtn = document.getElementById("Sendbtn");
-Confirmbtn.addEventListener("click", (Confirmation));
+// import { Confirmation } from "./confirmation.js";
+// const Confirmbtn = document.getElementById("Sendbtn");
+// Confirmbtn.addEventListener("click", (Confirmation));
 
 
+
+
+// const form = document.getElementById("Form");
+const name = document.getElementById('Name');
+const Cardnumber = document.getElementById('Cardnumber');
+const Month = document.getElementById('Month');
+const CVC = document.getElementById('CVC');
+const form = document.getElementById('Form');
+
+const errorName = document.getElementById('errorName');
+const error
+
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+
+    //jeżeli wszystko ok to wysyłamy
+    if (name.value.length >= 3) {
+        form.submit();
+    } else {
+        //jeżeli nie to pokazujemy jakieś błędy
+        errorName.innerHTML='blblb';
+    }
+
+    if (Cardnumber.value.length = 20 && Cardnumber.value.length = 20 ) {
+        form.submit();
+    } else {
+        //jeżeli nie to pokazujemy jakieś błędy
+        errorName.innerHTML='blblb';
+    }
+
+
+
+})
