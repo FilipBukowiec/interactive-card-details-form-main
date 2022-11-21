52db6ebd92140e9b74a69c5661aab57fc9b1cde8
Filip Bukowiec <filip.bukowiec@interia.pl>
2022-11-21 23:01:11 +0100

    finish validation

--- src/js/script.js
+++ src/js/script.js
@@ -9,6 +9,7 @@
 const name = document.getElementById('Name');
 const CardNumber = document.getElementById('Cardnumber');
 const Month = document.getElementById('Month');
+const Year = document.getElementById('Year');
 const CVC = document.getElementById('CVC');
 const form = document.getElementById('Form');
 const letters = /^[A-Za-z\s]+$/;
@@ -18,8 +19,10 @@ const all = letters + numbers;
 
 
 const errorName = document.getElementById('errorName');
-const errorCardNumber = document.getElementById('errorCardNumber')
-const errorCvc = document.getElementById('errorCvc')
+const errorCardNumber = document.getElementById('errorCardNumber');
+const errorMonth = document.getElementById('errorMonth');
+const errorYear = document.getElementById('errorYear');
+const errorCvc = document.getElementById('errorCvc');
 
 
 form.addEventListener("submit", e => {
@@ -34,33 +37,71 @@ form.addEventListener("submit", e => {
     } else {
         errorName.innerText = "This field should be a minimum of 3 characters, letters only";
         name.classList.replace('succes', 'error')
-        name.classList.add('error');}})
+        name.classList.add('error');
+    }
+})
 
 
-        form.addEventListener("submit", e => {
-            e.preventDefault();
-     if (CardNumber.value.length === 20 && CardNumber.value.match(numbers)) {
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    if (CardNumber.value.length === 20 && CardNumber.value.match(numbers)) {
         CardNumber.classList.replace('error', 'success')
         CardNumber.classList.add('success');
         errorCardNumber.innerText = "";
 
-    } else{
+    } else {
         errorCardNumber.innerText = "This field should be contains 20 characters, numbers only";
         CardNumber.classList.replace('succes', 'error')
         CardNumber.classList.add('error');
-    }});
+    }
+});
 
 
-    form.addEventListener("submit", e => {
-        e.preventDefault();
+form.addEventListener("submit", e => {
+    e.preventDefault();
 
-         if (CVC.value.length === 3 && CVC.value.match(numbers)) {
-            CVC.classList.replace('error', 'success')
-            CVC.classList.add('success');
-            errorCvc.innerText = "";
+    if (Month.value.length === 2 && Month.value.match(numbers)) {
+        Month.classList.replace('error', 'success');
+        Month.classList.add('success');
+        errorMonth.innerText = "";
 
-        }else{
-            CVC.classList.replace('succes', 'error')
-            CVC.classList.add('error');
-            errorCvc.innerText = "This field should be contains 3 characters, numbers only";
-    }})
+
+    } else {
+        Month.classList.replace('succes', 'error');
+        Month.classList.add('error');
+        errorMonth.innerText = "Enter the month correctly";
+
+    }
+})
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+
+    if (Year.value.length === 4 && Year.value.match(numbers)) {
+        Year.classList.replace('error', 'success');
+        Year.classList.add('success');
+        errorMonth.innerText = "";
+
+
+    } else {
+        Year.classList.replace('succes', 'error');
+        Year.classList.add('error');
+        errorYear.innerText = "Enter the year correctly";
+
+    }
+})
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+
+    if (CVC.value.length === 3 && CVC.value.match(numbers)) {
+        CVC.classList.replace('error', 'success');
+        CVC.classList.add('success');
+        errorCvc.innerText = "";
+
+    } else {
+        CVC.classList.replace('succes', 'error');
+        CVC.classList.add('error');
+        errorCvc.innerText = "This field should be contains 3 characters, numbers only";
+    }
+})
